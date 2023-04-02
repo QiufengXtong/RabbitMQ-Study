2df6363cb6ab301286deffdfce522e77fa5d36ca
QiufengXtong <qiufeng.xtong@gmail.com>
2023-04-02 14:54:45 +0800

    预取值示例代码

--- src/main/java/cn/xtong/example/message_answer/preset_value/Consumer09.java
+++ src/main/java/cn/xtong/example/message_answer/preset_value/Consumer09.java
@@ -2,13 +2,14 @@ package cn.xtong.example.message_answer.preset_value;
 
 import cn.hutool.core.thread.ThreadUtil;
 import cn.xtong.example.util.RabbitUtil;
-import com.rabbitmq.client.CancelCallback;
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.DeliverCallback;
 
 /**
- * 消费者Consumer09
+ * 预取值消费者Consumer09
  * 当Consumer08信道接受2条消息，Consumer09信道还没有接受够5条消息时，Consumer08信道暂时关闭，Consumer09接受够五条后开启
+ *
+ * @author 张晓童
+ * @date 2023/4/2 14:23
  */
 public class Consumer09 {
     public static final String QUEUE_NAME = "preset_value_queue";
